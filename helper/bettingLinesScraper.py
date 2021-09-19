93a90fe9fc7075a91a62a88630c135838bdbce4a
aunoyp <ap3441@columbia.edu>
2021-09-19 16:46:30 -0700

    Incorporate team records and standing into the game information

--- helper/bettingLinesScraper.py
+++ helper/bettingLinesScraper.py
@@ -10,9 +10,10 @@ from bs4 import BeautifulSoup
 from string import ascii_lowercase
 from collections import defaultdict
 from itertools import zip_longest
-from TeamNames import teamDict
+from TeamNames import teamDict, coversNames
 from tqdm.notebook import trange, tqdm
 from datetime import datetime
+from recordDateScraper import buildTable, getConferences
 
 def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
@@ -30,7 +31,9 @@ def getDate(text, ref_date, year):
 
 def getHomeAway(text, TEAM_NAME):
     at_char = '@' in text
-    OPP_NAME = text if not at_char else text.replace("@", "").strip()
+    OPP_STRING = text if not at_char else text.replace("@", "").strip()
+    OPP_NAMES = OPP_STRING.split()
+    OPP_NAME = OPP_NAMES[0]
 
     away = TEAM_NAME if at_char else OPP_NAME
     home = OPP_NAME if at_char else TEAM_NAME
@@ -50,7 +53,27 @@ def getSpread(text, isHome):
         text = text.replace(ats, "").strip()
     return (isHome*2 - 1) * float(text)
 
-def bettingLinesScraper(team_url, team_name, year, season_specs, meta_list):
+def getConferenceHelper(team_full, conf_list):
+    team_conf = 'eastern' if team_full in conf_list['eastern'] else ""
+    if(not team_conf):
+        team_conf = 'western' if team_full in conf_list['western'] else ""
+    if(not team_conf):
+        sys.exit('Conference Helper could not locate team in NBA reference table: ' +
+                   team_full)
+    return team_conf
+
+def getConference(home, away, conf_list):
+    home_full = teamDict[home]; away_full = teamDict[away]
+    home_conf = getConferenceHelper(home_full, conf_list)
+    away_conf = getConferenceHelper(away_full, conf_list)
+    return home_conf, away_conf
+
+def standardize_names(home, away):
+    home_name = home if home not in coversNames.keys() else coversNames[home]
+    away_name = away if away not in coversNames.keys() else coversNames[away]
+    return home_name, away_name
+
+def bettingLinesScraper(team_url, team_name, year, season_specs, records, conf_list, meta_list):
 
     response = requests.get(team_url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -60,7 +83,9 @@ def bettingLinesScraper(team_url, team_name, year, season_specs, meta_list):
 
     # Always have the spread for the Home team, against the spread
     # Do not include Over Under for now
-    keys = ['Date', 'Home_Team', 'Away_Team', 'Home_Score', 'Away_Score', 'Spread']
+    keys = ['Date', 'Home_Team', 'Away_Team', 'Home_Score',
+            'Away_Score', 'Spread', 'Home_Record', 'Away_Record',
+            'Home_Standing', 'Away_Standing']
     NUM_TABLE_ENTRIES = 5
 
     #### NEED TO GET TEAM NAME SOMEHOW
@@ -81,6 +106,7 @@ def bettingLinesScraper(team_url, team_name, year, season_specs, meta_list):
     for table in tables:
         ### Skip the process if table is not included in game type (reg season or playoffs)
         ### Pick the past results page tables for playoffs and reg season
+        ### tables is reversed because it is then goes chronologically
         if 'covers-CoversResults-Table' in table['class'] and len(table['class']) == 3:
             table_entries = grouper(table.find_all('td'), NUM_TABLE_ENTRIES)
             
@@ -97,7 +123,8 @@ def bettingLinesScraper(team_url, team_name, year, season_specs, meta_list):
                 date = getDate(entry[0].text.strip(), ref_date, year)
                 game['Date'] = date
 		# Scrape Home Team and Away Team
-                home, away = getHomeAway(entry[1].text.strip(), TEAM_NAME)
+                covers_home, covers_away = getHomeAway(entry[1].text.strip(), TEAM_NAME)
+                home, away = standardize_names(covers_home, covers_away)
                 game['Home_Team'] = home; game['Away_Team'] = away
 		# Scrape Scores and OT info
                 home_score, away_score, OT = getScore(entry[2].text.strip(),
@@ -109,6 +136,14 @@ def bettingLinesScraper(team_url, team_name, year, season_specs, meta_list):
                 game_meta =  (date, home, away) 
                 if game_meta not in meta_list:
                     games.append(game); new_meta.append(game_meta)
+        # Scrape Record
+                home_conf, away_conf = getConference(home, away, conf_list)
+                game['Home_Record'] = records[home_conf][date][home]['record']
+                game['Away_Record'] = records[away_conf][date][away]['record']
+                game['Home_Standing'] = str(records[home_conf][date][home]['standing']) + \
+                                        records[home_conf][date][home]['tie']
+                game['Away_Standing'] = str(records[away_conf][date][away]['standing']) + \
+                                        records[away_conf][date][away]['tie']
     return games, new_meta
 
 def scrape():
@@ -123,32 +158,40 @@ def scrape():
                    'P': ['Playoffs'],
                    'B': ['Regular Season', 'Playoffs']}
                    
-    start_year_input = input('\nInput starting year of season that you want ' +  
-                        'to start scraping from  (e.g. 2015 if start at 2015): ')
-    end_year_input = input('\nInput ending year of season that you want to ' + 
-                        'end scraping at (e.g. 2017 if scraping 2015-2016' + 
-                        'and 2016-2017 season')
-    try:
-        start_year = int(start_year_input)
-        end_year = int(end_year_input)
-    except:
-        sys.exit('Invalid Year')
-        
-    specs = input('\nScrape Regular Season (R), Playoff Games (P), or both (B): (default R)')
-    specs= 'R' if specs== '' else specs
+    #start_year_input = input('\nInput starting year of season that you want ' +
+    #                    'to start scraping from  (e.g. 2015 if start at 2015): ')
+    #end_year_input = input('\nInput ending year of season that you want to ' +
+    #                    'end scraping at (e.g. 2017 if scraping 2015-2016' +
+    #                    'and 2016-2017 season')
+    #try:
+    #    start_year = int(start_year_input)
+    #    end_year = int(end_year_input)
+    #except:
+    #    sys.exit('Invalid Year')
+    start_year = 2015
+    end_year = 2016
+    specs = 'R'
+    #specs = input('\nScrape Regular Season (R), Playoff Games (P), or both (B): (default R)')
+    #specs= 'R' if specs== '' else specs
     if specs not in game_types.keys():
         sys.exit('Invalid Game Specs')
         
     for year in trange(start_year, end_year, desc = 'Years', leave = True):
-        t = tqdm(teamDict.keys(), desc = 'Teams', leave = False) 
+        t = tqdm(teamDict.keys(), desc = 'Teams', leave = False)
+        print('Initializing Records for ' + str(year) + '...')
+        records = buildTable(year+1)
+        print('Getting conference lists ...')
+        conference_list = getConferences(year+1)
         for team_abbr in t:
             t.set_description(team_abbr)
             t.refresh()
             team_name = teamDict[team_abbr].lower()
             team_url = url + team_name.replace(" ", "-") + \
                     '/' + str(year) + '-' + str(year+1)
-            add_games, add_meta = bettingLinesScraper(team_url, team_abbr, 
-                                  year, game_types[specs], meta)
+        # Send in built table for record scraping
+            add_games, add_meta = bettingLinesScraper(team_url, team_abbr,
+                                  year, game_types[specs], records,
+                                  conference_list, meta)
 
             games = games + add_games; meta = meta + add_meta
     return games
